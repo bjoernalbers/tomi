6ece4dee6c9c5932058aa730b01fca5c24657e41
Björn Albers <bjoernalbers@gmail.com>
2024-12-18 13:28:04 +0100

    Fail if server address is missing.

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 // tomi
 package main
 
+import (
+	"flag"
+	"log"
+)
+
 func main() {
+	flag.Parse()
+	if len(flag.Args()) != 1 {
+		log.Fatal("Server address missing")
+	}
+	log.Print("Sorry, not implemened yet.")
 }
