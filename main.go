b6dd3d7f46fd9a26a1e3d2e71ccf825cfd465160
Björn Albers <bjoernalbers@gmail.com>
2025-01-03 08:15:27 +0100

    Extract function installTomedo().

--- main.go
+++ main.go
@@ -32,22 +32,29 @@ func main() {
 	if os.Geteuid() == 0 {
 		log.Fatal("please run as regular user, not as root or with sudo!")
 	}
+	if err := installTomedo(serverAddress); err != nil {
+		log.Fatalf("failed to install tomedo: %v", err)
+	}
+
+}
+
+func installTomedo(serverAddress string) error {
 	home := os.Getenv("HOME")
 	if home == "" {
-		log.Fatal("$HOME is not set")
+		return fmt.Errorf("$HOME is not set")
 	}
 	userAppsDir := filepath.Join(home, "Applications")
 	tomedoAppDir := filepath.Join(userAppsDir, "tomedo.app")
 	if _, err := os.Stat(tomedoAppDir); err == nil {
-		os.Exit(0)
+		return nil
 	}
 	// Create localized user applications directory if missing.
 	if _, err := os.Stat(userAppsDir); err != nil {
 		if err := os.Mkdir(userAppsDir, 0700); err != nil {
-			log.Fatalf("failed to create user applications directory: %v", err)
+			return fmt.Errorf("failed to create user applications directory: %v", err)
 		}
 		if f, err := os.Create(filepath.Join(userAppsDir, ".localized")); err != nil {
-			log.Fatalf("failed to localize user applications directory: %v", err)
+			return fmt.Errorf("failed to localize user applications directory: %v", err)
 		} else {
 			f.Close()
 		}
@@ -55,29 +62,30 @@ func main() {
 	// Download and unpack tomedo.
 	response, err := http.Get(tomedoDownloadURL(serverAddress))
 	if err != nil {
-		log.Fatalf("failed to download tomedo: %v", err)
+		return fmt.Errorf("failed to download tomedo: %v", err)
 	}
 	defer response.Body.Close()
 	if response.StatusCode != http.StatusOK {
-		log.Fatalf("failed to download tomedo: %d %s", response.StatusCode, http.StatusText(response.StatusCode))
+		return fmt.Errorf("failed to download tomedo: %d %s", response.StatusCode, http.StatusText(response.StatusCode))
 	}
 	tempFile, err := os.CreateTemp("", "tomedo.app.tar.*")
 	if err != nil {
-		log.Fatalf("failed to create temporary file: %v", err)
+		return fmt.Errorf("failed to create temporary file: %v", err)
 	}
 	defer os.Remove(tempFile.Name())
 	defer tempFile.Close()
 	if _, err := io.Copy(tempFile, response.Body); err != nil {
-		log.Fatalf("failed to download tomedo: %v", err)
+		return fmt.Errorf("failed to download tomedo: %v", err)
 	}
 	tar := exec.Command("/usr/bin/tar", "-x", "-f", tempFile.Name(), "-C", userAppsDir)
 	if output, err := tar.CombinedOutput(); err != nil {
-		log.Fatalf("failed to unpack tomedo: %s", string(output))
+		return fmt.Errorf("failed to unpack tomedo: %s", string(output))
 	}
 	// Add tomedo to Dock.
 	if err := addFileToDock(tomedoAppDir); err != nil {
-		log.Fatal(err)
+		return err
 	}
+	return nil
 }
 
 func Usage() {
