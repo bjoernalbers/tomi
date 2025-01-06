881ac11fae91c520b6144a5b3064e54cca478ade
Björn Albers <bjoernalbers@gmail.com>
2025-01-06 12:33:00 +0100

    Query latest Arzeko version based on architecture.

--- app/arzeko.go
+++ app/arzeko.go
@@ -1,13 +1,26 @@
 package app
 
-import "net/url"
+import (
+	"fmt"
+	"net/url"
+	"strings"
+)
 
 type Arzeko struct {
 	ServerURL *url.URL
+	Arch      string
 }
 
+// latestVersionURL returns the URL to query the download URL of the latest
+// Arzeko version.
 func (p *Arzeko) latestVersionURL() string {
 	u := p.ServerURL.JoinPath("arzeko/latestmac")
-	u.RawQuery = "version=0.0.0&aarch=intel"
+	// Arzeko itself queries latest version with "aarch=intel" on
+	// Intel-based Macs and "aarch=arm" on Apple Silicon.
+	arch := "intel"
+	if strings.HasPrefix(p.Arch, "arm") {
+		arch = "arm"
+	}
+	u.RawQuery = fmt.Sprintf("version=0.0.0&aarch=%s", arch)
 	return u.String()
 }
--- app/arzeko_test.go
+++ app/arzeko_test.go
@@ -3,9 +3,39 @@ package app
 import "testing"
 
 func TestArzekoLatestVersionURL(t *testing.T) {
-	arzeko := Arzeko{ServerURL: ServerURL()}
-	want := "http://tomedo.example.com:8080/tomedo_live/arzeko/latestmac?version=0.0.0&aarch=intel"
-	if got := arzeko.latestVersionURL(); got != want {
-		t.Fatalf("%#v.latestVersionURL():\ngot:\t%q\nwant:\t%q", arzeko, got, want)
+	tests := []struct {
+		arch string
+		want string
+	}{
+		{
+			"",
+			"http://tomedo.example.com:8080/tomedo_live/arzeko/latestmac?version=0.0.0&aarch=intel",
+		},
+		{
+			"intel",
+			"http://tomedo.example.com:8080/tomedo_live/arzeko/latestmac?version=0.0.0&aarch=intel",
+		},
+		{
+			"x86_64",
+			"http://tomedo.example.com:8080/tomedo_live/arzeko/latestmac?version=0.0.0&aarch=intel",
+		},
+		{
+			"amd64",
+			"http://tomedo.example.com:8080/tomedo_live/arzeko/latestmac?version=0.0.0&aarch=intel",
+		},
+		{
+			"arm",
+			"http://tomedo.example.com:8080/tomedo_live/arzeko/latestmac?version=0.0.0&aarch=arm",
+		},
+		{
+			"arm64",
+			"http://tomedo.example.com:8080/tomedo_live/arzeko/latestmac?version=0.0.0&aarch=arm",
+		},
+	}
+	for _, tt := range tests {
+		arzeko := Arzeko{ServerURL: ServerURL(), Arch: tt.arch}
+		if got := arzeko.latestVersionURL(); got != tt.want {
+			t.Errorf("%#v.latestVersionURL():\ngot:\t%q\nwant:\t%q", arzeko, got, tt.want)
+		}
 	}
 }
