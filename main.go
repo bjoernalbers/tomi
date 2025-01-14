1688f64031535599f51fcfbafd5cc6b2f021f102
Björn Albers <bjoernalbers@gmail.com>
2025-01-14 16:06:58 +0100

    Perform app configuration before Dock update.

--- main.go
+++ main.go
@@ -62,10 +62,10 @@ func main() {
 		if err := app.Install(a); err != nil {
 			log.Fatalf("%s: %v", a.Name(), err)
 		}
-		if err := dock.Add(a.Path()); err != nil {
+		if err := a.Configure(); err != nil {
 			log.Fatalf("%s: %v", a.Name(), err)
 		}
-		if err := a.Configure(); err != nil {
+		if err := dock.Add(a.Path()); err != nil {
 			log.Fatalf("%s: %v", a.Name(), err)
 		}
 	}
