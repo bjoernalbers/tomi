a07cf062466b62d255c0bb7f87a01a9e0432673f
Björn Albers <bjoernalbers@gmail.com>
2025-01-22 12:17:02 +0100

    Extract "pkg" package.

--- main.go
+++ main.go
@@ -4,15 +4,12 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io"
 	"log"
 	"os"
-	"os/exec"
-	"path/filepath"
 	"runtime"
-	"strings"
 
 	"github.com/bjoernalbers/tomi/macos"
+	"github.com/bjoernalbers/tomi/pkg"
 	"github.com/bjoernalbers/tomi/tomedo"
 )
 
@@ -38,7 +35,7 @@ func main() {
 		log.Fatal("please run as regular user, not as root or with sudo!")
 	}
 	if *buildPackage {
-		if err := buildPkg(version); err != nil {
+		if err := pkg.Build(version); err != nil {
 			log.Fatalf("build package: %v", err)
 		}
 		os.Exit(0)
@@ -91,67 +88,4 @@ Usage: tomi [options]
 Options:`, version)
 	fmt.Fprintln(flag.CommandLine.Output(), header)
 	flag.PrintDefaults()
-}
-
-func buildPkg(version string) error {
-	payloadDir, err := os.MkdirTemp("", "payload-*")
-	if err != nil {
-		return fmt.Errorf("create payload dir: %v", err)
-	}
-	defer os.RemoveAll(payloadDir)
-	scriptsDir, err := os.MkdirTemp("", "scripts-*")
-	if err != nil {
-		return fmt.Errorf("create scripts dir: %v", err)
-	}
-	defer os.RemoveAll(scriptsDir)
-	preinstall, err := os.OpenFile(filepath.Join(scriptsDir, "preinstall"), os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0777)
-	if err != nil {
-		return fmt.Errorf("create preinstall script: %v", err)
-	}
-	script := `#!/bin/sh
-
-set -eu
-
-export PATH=/bin:/sbin:/usr/bin:/usr/sbin
-
-tomi="$(dirname "$0")/tomi"
-
-loggedInUser() {
-	local user=$(stat -f %Su /dev/console)
-
-	if [ -z "${user}" ]; then exit 1; fi
-	if [ "${user}" = 'root' ]; then exit 1; fi
-	echo "${user}"
-}
-
-if username=$(loggedInUser); then
-	echo "Running tomi as user '${username}'..."
-	sudo -u "${username}" --set-home "${tomi}"
-else
-	echo "ERROR: Could not determine active user."
-	exit 1
-fi
-`
-	if _, err := io.Copy(preinstall, strings.NewReader(script)); err != nil {
-		return fmt.Errorf("copy preinstall script: %v", err)
-	}
-	preinstall.Close()
-	tomi, err := os.Executable()
-	if err != nil {
-		return fmt.Errorf("get tomi executable: %v", err)
-	}
-	if output, err := exec.Command("/bin/cp", "-p", tomi, filepath.Join(scriptsDir, "tomi")).CombinedOutput(); err != nil {
-		return fmt.Errorf("copy tomi: %v", string(output))
-	}
-	cmd := exec.Command("/usr/bin/pkgbuild",
-		"--identifier", "de.bjoernalbers.tomedo-installer",
-		"--version", version,
-		"--scripts", scriptsDir,
-		"--root", payloadDir,
-		"tomedo-installer.pkg")
-	output, err := cmd.CombinedOutput()
-	if err != nil {
-		return fmt.Errorf("%s", string(output))
-	}
-	return nil
 }
---
+++ pkg/pkg.go
@@ -0,0 +1,73 @@
+package pkg
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
+
+func Build(version string) error {
+	payloadDir, err := os.MkdirTemp("", "payload-*")
+	if err != nil {
+		return fmt.Errorf("create payload dir: %v", err)
+	}
+	defer os.RemoveAll(payloadDir)
+	scriptsDir, err := os.MkdirTemp("", "scripts-*")
+	if err != nil {
+		return fmt.Errorf("create scripts dir: %v", err)
+	}
+	defer os.RemoveAll(scriptsDir)
+	preinstall, err := os.OpenFile(filepath.Join(scriptsDir, "preinstall"), os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0777)
+	if err != nil {
+		return fmt.Errorf("create preinstall script: %v", err)
+	}
+	script := `#!/bin/sh
+
+set -eu
+
+export PATH=/bin:/sbin:/usr/bin:/usr/sbin
+
+tomi="$(dirname "$0")/tomi"
+
+loggedInUser() {
+	local user=$(stat -f %Su /dev/console)
+
+	if [ -z "${user}" ]; then exit 1; fi
+	if [ "${user}" = 'root' ]; then exit 1; fi
+	echo "${user}"
+}
+
+if username=$(loggedInUser); then
+	echo "Running tomi as user '${username}'..."
+	sudo -u "${username}" --set-home "${tomi}"
+else
+	echo "ERROR: Could not determine active user."
+	exit 1
+fi
+`
+	if _, err := io.Copy(preinstall, strings.NewReader(script)); err != nil {
+		return fmt.Errorf("copy preinstall script: %v", err)
+	}
+	preinstall.Close()
+	tomi, err := os.Executable()
+	if err != nil {
+		return fmt.Errorf("get tomi executable: %v", err)
+	}
+	if output, err := exec.Command("/bin/cp", "-p", tomi, filepath.Join(scriptsDir, "tomi")).CombinedOutput(); err != nil {
+		return fmt.Errorf("copy tomi: %v", string(output))
+	}
+	cmd := exec.Command("/usr/bin/pkgbuild",
+		"--identifier", "de.bjoernalbers.tomedo-installer",
+		"--version", version,
+		"--scripts", scriptsDir,
+		"--root", payloadDir,
+		"tomedo-installer.pkg")
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("%s", string(output))
+	}
+	return nil
+}
